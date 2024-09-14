39ec9bcb220adc6757b2bada8a1ad9f4520e3212
Jarsters <kepalabotak9876@gmail.com>
2024-09-14 17:51:12 +0700

    Packet errornya ketinggalan XD (relay)

--- relay/utils/message.py
+++ relay/utils/message.py
@@ -15,6 +15,6 @@ def send_packet_error(communicate, code_error, initiator):
         "error": True,
         "body": "User not found or User not online"
     }
-    print(f"Packet error untuk user {initiator}")
+    print(f"Packet error untuk user {initiator}\r\n{packet}")
     if(code_error == 404):
         send_message(communicate, packet)
