1143228590c61ba76c5eed16b5e6e056236c64e8
Jarsters <kepalabotak9876@gmail.com>
2024-09-30 21:40:03 +0700

    Debugging permasalahan komponen manager

--- programs/manager/manager_utils/import_abs_path.py
+++ programs/manager/manager_utils/import_abs_path.py
@@ -3,5 +3,7 @@ from importlib.machinery import SourceFileLoader
 original_path = os.path.dirname(os.path.abspath(__file__))
 top_one_path  = original_path[:-len("manager\\manager_utils")]
 
+print(f"Top one path manager: {top_one_path}")
+
 def import_outside_utils(outside_folder, file_name):
     return SourceFileLoader("", top_one_path + outside_folder + file_name).load_module()
--- programs/tracker/tracker_utils/import_abs_path.py
+++ programs/tracker/tracker_utils/import_abs_path.py
@@ -3,5 +3,7 @@ from importlib.machinery import SourceFileLoader
 original_path = os.path.dirname(os.path.abspath(__file__))
 top_one_path  = original_path[:-len("tracker\\tracker_utils")]
 
+print(f"Top one path tracker: {top_one_path}")
+
 def import_outside_utils(outside_folder, file_name):
     return SourceFileLoader("", top_one_path + outside_folder + file_name).load_module()
